75ea0144ceb75dbb171e508cedea73dc4a36e689
Jonathan Fong <robofin117@gmail.com>
2018-12-01 15:32:59 -0800

    Fixed project facet.

--- TextEncryptor/src/textEncryptorClient/RESTTextEncryptorClient.java
+++ TextEncryptor/src/textEncryptorClient/RESTTextEncryptorClient.java
@@ -1,10 +1,35 @@
 package textEncryptorClient;
 
-public class RESTTextEncryptorClient {
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class RESTTextEncryptorClient extends Application{
+	
+	Button encryptButton = new Button("Encrypt");
+	Button decryptButton = new Button("Decrypt");
+	TextField textToEncrypt = new TextField();
+	TextField dataToDecrypt = new TextField();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		VBox vbox = new VBox(10, encryptButton, decryptButton);
+		primaryStage.setTitle("Text Encryptor");
+		Scene myScene = new Scene (vbox);
+		
+		
+		primaryStage.show();
 	}
 
 }
