3fc33a28efda5d4451666a3ab425a5fbbec1bd87
Jonathan Fong <robofin117@gmail.com>
2018-12-01 15:53:19 -0800

    GUI additions.

--- TextEncryptor/src/textEncryptorClient/RESTTextEncryptorClient.java
+++ TextEncryptor/src/textEncryptorClient/RESTTextEncryptorClient.java
@@ -5,7 +5,9 @@ package textEncryptorClient;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,6 +17,9 @@ public class RESTTextEncryptorClient extends Application{
 	Button decryptButton = new Button("Decrypt");
 	TextField textToEncrypt = new TextField();
 	TextField dataToDecrypt = new TextField();
+	
+	Label encryptText = new Label("Text To Encrypt");
+	Label decryptText = new Label("Data to Decrypt");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -24,11 +29,16 @@ public class RESTTextEncryptorClient extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		VBox vbox = new VBox(10, encryptButton, decryptButton);
+		
+		HBox hbox1 = new HBox(10, encryptButton, decryptButton);
+		HBox hbox2 = new HBox(10, textToEncrypt, dataToDecrypt);
+		
+		VBox vbox = new VBox(10, hbox2, hbox1);
+		
 		primaryStage.setTitle("Text Encryptor");
 		Scene myScene = new Scene (vbox);
 		
-		
+		primaryStage.setScene(myScene);
 		primaryStage.show();
 	}
 
