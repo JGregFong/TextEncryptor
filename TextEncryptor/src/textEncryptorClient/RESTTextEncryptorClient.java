a0d782e8eee209c3a404d001c84dab87e955d857
Jonathan Fong <robofin117@gmail.com>
2018-12-01 16:49:45 -0800

    Setup and adding dependencies.

--- TextEncryptor/src/textEncryptorClient/RESTTextEncryptorClient.java
+++ TextEncryptor/src/textEncryptorClient/RESTTextEncryptorClient.java
@@ -3,6 +3,7 @@ package textEncryptorClient;
 
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -18,8 +19,8 @@ public class RESTTextEncryptorClient extends Application{
 	TextField textToEncrypt = new TextField();
 	TextField dataToDecrypt = new TextField();
 	
-	Label encryptText = new Label("Text To Encrypt");
-	Label decryptText = new Label("Data to Decrypt");
+	Label encryptText = new Label("Text To Encrypt ->");
+	Label decryptText = new Label("<- Data to Decrypt");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -32,14 +33,32 @@ public class RESTTextEncryptorClient extends Application{
 		
 		HBox hbox1 = new HBox(10, encryptButton, decryptButton);
 		HBox hbox2 = new HBox(10, textToEncrypt, dataToDecrypt);
+		HBox hbox3 = new HBox(10, encryptText, decryptText);
 		
-		VBox vbox = new VBox(10, hbox2, hbox1);
+		VBox vbox = new VBox(10, hbox3, hbox2, hbox1);
 		
 		primaryStage.setTitle("Text Encryptor");
-		Scene myScene = new Scene (vbox);
+		Scene myScene = new Scene (vbox, 500,300);
+		
+		vbox.setAlignment(Pos.CENTER);
+		hbox1.setAlignment(Pos.CENTER);
+		hbox2.setAlignment(Pos.CENTER);
+		hbox3.setAlignment(Pos.CENTER);
+		
+		encryptButton.setOnAction(event ->{
+			
+		});
+		
+		decryptButton.setOnAction(event-> {
+			
+		});
 		
 		primaryStage.setScene(myScene);
 		primaryStage.show();
+	}
+	
+	public void transmit(boolean hasEncryption, String message) {
+		
 	}
 
 }
