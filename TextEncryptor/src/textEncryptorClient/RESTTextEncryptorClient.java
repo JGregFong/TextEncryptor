b17158b70f641d0d67e4c81111d096cdeb790839
Jonathan Fong <robofin117@gmail.com>
2018-12-09 16:30:38 -0800

    Updates to the TextEncryptor project.

--- TextEncryptor/src/textEncryptorClient/RESTTextEncryptorClient.java
+++ TextEncryptor/src/textEncryptorClient/RESTTextEncryptorClient.java
@@ -19,6 +19,8 @@ import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.json.JSONObject;
+
 public class RESTTextEncryptorClient extends Application{
 	
 	Button encryptButton = new Button("Encrypt");
@@ -52,11 +54,25 @@ public class RESTTextEncryptorClient extends Application{
 		hbox2.setAlignment(Pos.CENTER);
 		hbox3.setAlignment(Pos.CENTER);
 		
+		
+		
 		encryptButton.setOnAction(event ->{
+			
+			String message = textToEncrypt.getText();
+			
+			String result = transmit(true, message);
+			
+			dataToDecrypt.setText(result);
 			
 		});
 		
 		decryptButton.setOnAction(event-> {
+			
+			String message = dataToDecrypt.getText();
+			
+			String result = transmit(false, message);
+			
+			textToEncrypt.setText(result);
 			
 		});
 		
@@ -64,14 +80,69 @@ public class RESTTextEncryptorClient extends Application{
 		primaryStage.show();
 	}
 	
-	public String transmit(boolean hasEncryption, String message) {
+	public String transmit(boolean getsEncryption, String message) {
 		String result = "";
-		if(hasEncryption) {
-		
-		}
-		else {
+		try {
+			
+			
+			if(getsEncryption) {
+				try {
+					URL encryptURL = new URL("http://localhost:8080/TextEncryptor/api/encryptor");
+					URLConnection connection = encryptURL.openConnection();
+					connection.setDoOutput(true);
+					connection.setRequestProperty("Content-Type", "application/json");
+					connection.setConnectTimeout(5000);
+					connection.setReadTimeout(5000);
+					OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+					out.write(message);
+					out.close();
+					
+					BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+					String received = null;
+					while ((received = in.readLine()) != null) {
+						System.out.println(received);
+						result = received;
+					}
+					System.out.println("Encrypted Message Received.");
+					in.close();
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+			else {
+				try {
+					URL decryptURL = new URL("http://localhost:8080/TextEncryptor/api/decryptor");
+					URLConnection connection = decryptURL.openConnection();
+					connection.setDoOutput(true);
+					connection.setRequestProperty("Content-Type", "application/json");
+					connection.setConnectTimeout(5000);
+					connection.setReadTimeout(5000);
+					OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+					out.write(message);
+					out.close();
+					
+					BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+					String received = null;
+					while ((received = in.readLine()) != null) {
+						System.out.println(received);
+						result = received;
+					}
+					System.out.println("Decrypted Message Received.");
+					in.close();
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}			
+			
 			
 		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
 		return result;
 	}
 
--- TextEncryptor/src/textEncryptorServer/RESTTextEncryptorServer.java
+++ TextEncryptor/src/textEncryptorServer/RESTTextEncryptorServer.java
@@ -4,6 +4,14 @@ package textEncryptorServer;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+ 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
  
 import javax.print.attribute.standard.Media;
 import javax.ws.rs.Consumes;
@@ -14,57 +22,75 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+
 @Path("/")
+
 public class RESTTextEncryptorServer {
+	
+	final String encryptionKey = "Merry Christmas, Professor!";
 
 	@POST
 	@Path("/encryptor")
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.TEXT_PLAIN)
 	public Response encryptREST(InputStream incomingData) {
-		StringBuilder crunchifyBuilder = new StringBuilder();
+		StringBuilder originalMessage = new StringBuilder();
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
 			String line = null;
 			while ((line = in.readLine()) != null) {
-				crunchifyBuilder.append(line);
+				originalMessage.append(line);
 			}
 		} catch (Exception e) {
 			System.out.println("Error Parsing: - ");
 		}
-		System.out.println("Data Received: " + crunchifyBuilder.toString());
- 
+		
+		
+		
+		System.out.println("Data Received: " + originalMessage.toString());
+		String encryptedMessage = AES.encrypt(originalMessage.toString(), encryptionKey);
+		
+		System.out.println("Data Encrypted: " + encryptedMessage);
+		
 		// return HTTP response 200 in case of success
-		return Response.status(200).entity(crunchifyBuilder.toString()).build();
+		return Response.status(200).entity(encryptedMessage).build();
 	}
  
 	@POST
 	@Path("/decryptor")
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.TEXT_PLAIN)
 	public Response decryptREST(InputStream incomingData) {
-		StringBuilder crunchifyBuilder = new StringBuilder();
+		StringBuilder encryptedMessage = new StringBuilder();
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
 			String line = null;
 			while ((line = in.readLine()) != null) {
-				crunchifyBuilder.append(line);
+				encryptedMessage.append(line);
 			}
+			
+			
+			
 		} catch (Exception e) {
 			System.out.println("Error Parsing: - ");
 		}
-		System.out.println("Data Received: " + crunchifyBuilder.toString());
- 
+		System.out.println("Data Received: " + encryptedMessage.toString());
+		String decryptedMessage = AES.decrypt(encryptedMessage.toString(), encryptionKey);
+		
+		System.out.println("Data Decrypted: "+ decryptedMessage);
+		
 		// return HTTP response 200 in case of success
-		return Response.status(200).entity(crunchifyBuilder.toString()).build();
+		return Response.status(200).entity(decryptedMessage).build();
 	}
 	
 	@GET
 	@Path("/verify")
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response verifyRESTService(InputStream incomingData) {
-		String result = "CrunchifyRESTService Successfully started..";
+		String result = "Service is working";
  
 		// return HTTP response 200 in case of success
 		return Response.status(200).entity(result).build();
 	}
+	
+
 	
 }
